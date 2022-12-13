2e7cb8058435a3b843e9f9dfdc9c7515afc5d6e1
Aurelian-C <aurelian_reghin@yahoo.com>
2022-12-13 09:16:37 +0200

    update

--- src/js/views/tooltipView.js
+++ src/js/views/tooltipView.js
@@ -4,22 +4,22 @@ class TooltipView {
   constructor() {
     document.body.addEventListener('mouseover', function (e) {
       const hasToolClass = e.target.classList.contains('tooltip');
+
       if (!hasToolClass) return;
 
       const title = e.target.closest('.card__article-title');
-      console.log(title);
       const obj = allTitlesAsObjects.find(el => {
         return el.sectionTitle === title.textContent;
       });
+
       if (!obj.tooltips) return;
-      console.log(obj.tooltips);
+
       const tooltipText = obj.tooltips
         .map(string => `<p class="tooltip_paragraph">${string}</p>`)
         .join('');
       const insertTooltip = `<div class="tooltip__text">${tooltipText}</div>`;
 
       e.target.insertAdjacentHTML('afterbegin', insertTooltip);
-      console.log('work');
     });
   }
 }
