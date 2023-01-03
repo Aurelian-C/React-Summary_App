759821bb694b3a480ac19bd89524d4ba626e6001
Aurelian-C <aurelian_reghin@yahoo.com>
2023-01-03 13:41:41 +0200

    update

--- src/js/model/modelDataStorage.js
+++ src/js/model/modelDataStorage.js
@@ -1193,7 +1193,7 @@ const section13 = {
       },
       tooltips: [
         'Error boundaries are React components that catch JavaScript errors anywhere in their child component tree, log those errors, and display a fallback UI instead of the component tree that crashed.',
-        "Error boundaries work like a JavaScript <code>catch {}>/code> block, but for components. Only class components can be error boundaries. In practice, most of the time you'll want to declare an error boundary component once and use it throughout your application.",
+        "Error boundaries work like a JavaScript <code>catch {}</code> block, but for components. Only class components can be error boundaries. In practice, most of the time you'll want to declare an error boundary component once and use it throughout your application.",
         "Note that error boundaries only catch errors in the components below them in the tree. An error boundary can't catch an error within itself.",
       ],
     },
