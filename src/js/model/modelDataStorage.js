4f300652cfa58c2f388b6aba4fedfbb3e4e5afa7
Aurelian-C <aurelian_reghin@yahoo.com>
2023-03-10 12:54:59 +0200

    update

--- src/js/model/modelDataStorage.js
+++ src/js/model/modelDataStorage.js
@@ -110,9 +110,9 @@ const React_basics_and_working_with_components = {
     },
     {
       sectionTitle:
-        'Outputting dynamic data & working with expressions in JSX markup',
+        'JavaScript in JSX with Curly Braces: Outputting dynamic data & working with expressions in JSX markup',
       sectionSource:
-        '/src/markdowns/01_React_basics_&_working_with_Components/Outputting_dynamic_data_&_working_with_expressions.html',
+        '/src/markdowns/01_React_basics_&_working_with_Components/JavaScript_in_JSX_with_Curly_Braces.html',
       highlights: {
         highlight1: ['Outputting dynamic data'],
         highlight2: ['expressions in JSX'],
